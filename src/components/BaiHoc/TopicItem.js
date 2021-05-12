978c7b33f1b61ea2a0c1e8296a93869f6b9eceea
VincentLong73 <longngodaugo.1202@gmail.com>
2021-05-12 22:16:06 +0700

    edit keyboard in soanthao

--- src/components/BaiHoc/TopicItem.js
+++ src/components/BaiHoc/TopicItem.js
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 import { ProgressBar } from "react-bootstrap";
 
 function TopicItem(props) {
-    const { title, lessions, numberOfCompletedLessions } = props.topic;
+    const { index, title, lessions, numberOfCompletedLessions } = props.topic;
     const progress = parseInt(
         (numberOfCompletedLessions / lessions.length) * 100
     );
+    console.log(props.key)
 
     return (
         <li className="topic-item">
             <Link to="/baihoc/1">
+                <h3 className="course-index main-text-color fw-bold">Bài {index}:</h3>
                 <h3 className="title">{title}</h3>
                 <div className="topic-content">
                     <ul className="topic-content__lessions">
@@ -21,9 +23,15 @@ function TopicItem(props) {
                     </ul>
                     <div className="progress-wrapper">
                         <p>
-                            Hoàn thành: {numberOfCompletedLessions}/
-                            {lessions.length}
+                            <span className="mr-1">
+                                Hoàn thành: {numberOfCompletedLessions}/{lessions.length} 
+                            </span>
+                            <span>
+                                {(numberOfCompletedLessions/lessions.length) == 1 && <i className="fas fa-check-circle"></i>}
+                            </span>
+                            
                         </p>
+                        <p></p>
                         <ProgressBar now={progress} />
                     </div>
                 </div>
