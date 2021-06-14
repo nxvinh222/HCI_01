9120af7eba74d0264ae14032864a30e88f428b04
Vinh <nxvinh222@gmail.com>
2021-06-14 14:00:40 +0700

    edit avatar

--- src/components/thidau/FindingPlayers.js
+++ src/components/thidau/FindingPlayers.js
@@ -15,15 +15,24 @@ function FindingPlayers(props) {
                 <h3>Đang tìm kiếm người chơi</h3>
                 <ul className="pending-players">
                     <li className="pending-player">
-                        <img src={player1} alt="" />
+                        <img
+                            src="https://store.playstation.com/store/api/chihiro/00_09_000/container/TW/en/99/HP0700-CUSA08526_00-PREMIUMAVATAR007/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=720&h=720"
+                            alt=""
+                        />
                         <MdCheckCircle color="#28A745" size="1.5em" />
                     </li>
                     <li className="pending-player">
-                        <img src={player2} alt="" />
+                        <img
+                            src="https://store.playstation.com/store/api/chihiro/00_09_000/container/TW/en/99/HP0700-CUSA08526_00-PREMIUMAVATAR007/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=720&h=720"
+                            alt=""
+                        />
                         <MdCheckCircle color="#28A745" size="1.5em" />
                     </li>
                     <li id="main-player" className="pending-player">
-                        <img src={avatar} alt="" />
+                        <img
+                            src="https://store.playstation.com/store/api/chihiro/00_09_000/container/TW/en/99/HP0700-CUSA08526_00-PREMIUMAVATAR007/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=720&h=720"
+                            alt=""
+                        />
                         <Spinner animation="border" variant="success" />
                         <button
                             className="button-primary"
@@ -35,11 +44,17 @@ function FindingPlayers(props) {
                         </button>
                     </li>
                     <li className="pending-player">
-                        <img src={player3} alt="" />
+                        <img
+                            src="https://store.playstation.com/store/api/chihiro/00_09_000/container/TW/en/99/HP0700-CUSA08526_00-PREMIUMAVATAR007/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=720&h=720"
+                            alt=""
+                        />
                         <MdCheckCircle color="#28A745" size="1.5em" />
                     </li>
                     <li className="pending-player">
-                        <img src={player4} alt="" />
+                        <img
+                            src="https://store.playstation.com/store/api/chihiro/00_09_000/container/TW/en/99/HP0700-CUSA08526_00-PREMIUMAVATAR007/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=720&h=720"
+                            alt=""
+                        />
                         <Spinner animation="border" variant="success" />
                     </li>
                 </ul>
